1076360916e2f932c2ce6f2aa69bcfcc1990e493
Saecki <tobiasschmitz2001@gmail.com>
2021-04-12 19:54:21 +0200

    Add collapsing header layout in album view

---
+++ app/src/main/java/agersant/polaris/features/browse/BrowseItemHolderDiscography.kt
@@ -0,0 +1,42 @@
+package agersant.polaris.features.browse
+
+import agersant.polaris.CollectionItem
+import agersant.polaris.PlaybackQueue
+import agersant.polaris.R
+import agersant.polaris.api.API
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+
+internal class BrowseItemHolderDiscography(
+    api: API?,
+    playbackQueue: PlaybackQueue?,
+    adapter: BrowseAdapter?,
+    itemView: View,
+    itemQueueStatusView: View?
+) : BrowseItemHolder(api, playbackQueue, adapter, itemView, itemQueueStatusView) {
+
+    private val artwork: ImageView = itemView.findViewById(R.id.artwork)
+    private val artist: TextView = itemView.findViewById(R.id.artist)
+    private val album: TextView = itemView.findViewById(R.id.album)
+
+    init {
+        itemView.setOnClickListener(this)
+    }
+
+    public override fun bindItem(item: CollectionItem) {
+        super.bindItem(item)
+
+        var artistString = item.albumArtist ?: item.artist ?: ""
+        if (item.year != -1) {
+            artistString += " • ${item.year}"
+        }
+        artist.text = artistString
+        album.text = item.album ?: ""
+        if (item.artwork != null) {
+            api.loadImageIntoView(item, artwork)
+        } else {
+            artwork.setImageResource(R.drawable.ic_fallback_artwork)
+        }
+    }
+}
