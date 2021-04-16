7a5f4dd13c1687bc5fa24b42eb839bb297f4aee9
saecki <tobiasschmitz2001@gmail.com>
2021-04-16 18:52:33 +0200

    add fast scrolling bar

--- app/src/main/java/agersant/polaris/features/browse/BrowseViewDiscography.java
+++ app/src/main/java/agersant/polaris/features/browse/BrowseViewDiscography.java
@@ -34,7 +34,6 @@ class BrowseViewDiscography extends BrowseViewContent {
 
         RecyclerView recyclerView = findViewById(R.id.browse_recycler_view);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         ItemTouchHelper.Callback callback = new BrowseTouchCallback();
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(callback);
