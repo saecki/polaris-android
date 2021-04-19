f3345fcefd4c1f9c57f94fec2d4c103f213ba9db
saecki <tobiasschmitz2001@gmail.com>
2021-04-19 23:50:09 +0200

    small tweaks to the album view

--- app/src/main/java/agersant/polaris/features/browse/BrowseViewAlbum.kt
+++ app/src/main/java/agersant/polaris/features/browse/BrowseViewAlbum.kt
@@ -6,20 +6,18 @@ import agersant.polaris.R
 
 import agersant.polaris.api.API
 import agersant.polaris.databinding.ViewBrowseAlbumBinding
+import agersant.polaris.util.getAttrColor
 import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.ColorStateList
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
+import android.content.res.Configuration
 import android.graphics.drawable.GradientDrawable
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.core.os.postDelayed
 import androidx.palette.graphics.Palette
@@ -66,33 +64,35 @@ internal class BrowseViewAlbum(
         adapter = BrowseAdapterAlbum(api, playbackQueue)
         recyclerView.adapter = adapter
 
-        recyclerView.setOnScrollChangeListener { v, _, _, _, _ ->
-            if (v.canScrollVertically(-1)) {
-                divider?.visibility = VISIBLE
-            } else {
-                divider?.visibility = INVISIBLE
+        when (resources.configuration.orientation) {
+            Configuration.ORIENTATION_PORTRAIT -> {
+                recyclerView.setOnScrollChangeListener { v, _, _, _, _ ->
+                    if (v.canScrollVertically(-1)) {
+                        divider?.visibility = VISIBLE
+                    } else {
+                        divider?.visibility = INVISIBLE
+                    }
+                }
+            }
+            Configuration.ORIENTATION_LANDSCAPE -> {
+                recyclerView.setOnScrollChangeListener { _, _, _, _, oldScrollY ->
+                    if (oldScrollY < 0) {
+                        queueAll.shrink()
+                    } else if (oldScrollY > 0) {
+                        queueAll.extend()
+                    }
+                }
             }
         }
 
         motionLayout?.addTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) = Unit
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, progress: Float) = Unit
             override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) = Unit
             override fun onTransitionCompleted(p0: MotionLayout?, id: Int) {
-                if (id == R.id.start) {
-                    title.textAlignment = TEXT_ALIGNMENT_CENTER
-                    queueAll.extend()
-                } else {
-                    title.textAlignment = TEXT_ALIGNMENT_TEXT_START
-                    queueAll.shrink()
-                }
-            }
-
-            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) = Unit
-
-            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, progress: Float) {
-                if (progress > 0.8f) {
-                    title.textAlignment = TEXT_ALIGNMENT_TEXT_START
-                } else {
-                    title.textAlignment = TEXT_ALIGNMENT_CENTER
+                when (id) {
+                    R.id.start -> queueAll.extend()
+                    R.id.end -> queueAll.shrink()
                 }
             }
         })
@@ -100,7 +100,7 @@ internal class BrowseViewAlbum(
         queueAll.setOnClickListener {
             playbackQueue.addItems(adapter.items)
             queueAll.setIconResource(R.drawable.ic_check_24)
-            Handler().postDelayed(1000) {
+            handler.postDelayed(1000) {
                 queueAll.setIconResource(R.drawable.ic_playlist_play_24)
             }
         }
@@ -125,50 +125,57 @@ internal class BrowseViewAlbum(
         artist.text = artistString
         title.text = item.album ?: ""
         if (item.artwork != null) {
-            api.loadImageIntoView(item, artwork, this::animateAccentColorChange)
+            api.loadImageIntoView(item, artwork) { image ->
+                Palette.from(image).generate { palette ->
+                    val swatch = palette?.run {
+                        dominantSwatch
+                            ?: mutedSwatch
+                    }
+                    swatch?.run {
+                        animateAccentColorChange(rgb, bodyTextColor)
+                    }
+                }
+            }
         } else {
             artwork.setImageResource(R.drawable.ic_fallback_artwork)
+            val bgColor = context.getAttrColor(R.attr.colorPrimary)
+            val fgColor = context.getAttrColor(R.attr.colorOnPrimary)
+            animateAccentColorChange(bgColor, fgColor)
         }
     }
 
-    private fun animateAccentColorChange(image: Bitmap) {
-        Palette.from(image).generate { palette ->
-            val swatch = palette?.run {
-                dominantSwatch
-                    ?: mutedSwatch
-            }
-
-            swatch?.run {
-                val animator = ValueAnimator.ofFloat(0f, 1f)
-                val evaluator = ArgbEvaluator()
-                var gradientDrawable: GradientDrawable? = null
-                headerBackground?.let {
-                    val drawable = GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, intArrayOf(0, 0))
-                    drawable.alpha = 0xA0
-                    it.background = drawable
-                    gradientDrawable = drawable
-                }
-                sidebarBackground?.let {
-                    val drawable = GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, intArrayOf(0, 0))
-                    drawable.alpha = 0xA0
-                    it.background = drawable
-                    gradientDrawable = drawable
-                }
-                val fabInitBg = queueAll.backgroundTintList?.defaultColor ?: 0
-                val fabInitFg = queueAll.currentTextColor
-                animator.addUpdateListener {
-                    val f = it.animatedFraction
-                    val bg = evaluator.evaluate(f, 0, rgb) as Int
-                    val fabBg = evaluator.evaluate(f, fabInitBg, rgb) as Int
-                    val fabFg = evaluator.evaluate(f, fabInitFg, bodyTextColor) as Int
-                    gradientDrawable?.colors = intArrayOf(bg, 0)
-                    queueAll.background.setTint(fabBg)
-                    queueAll.setTextColor(fabFg)
-                    queueAll.iconTint = ColorStateList.valueOf(fabFg)
-                }
-                animator.duration = 400
-                animator.start()
-            }
+    private fun animateAccentColorChange(bgColor: Int, fgColor: Int) {
+        val animator = ValueAnimator.ofFloat(0f, 1f)
+        val evaluator = ArgbEvaluator()
+        var gradientDrawable: GradientDrawable? = null
+        headerBackground?.let {
+            val drawable = GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, intArrayOf(0, 0))
+            drawable.alpha = 0xA0
+            it.background = drawable
+            gradientDrawable = drawable
         }
+        sidebarBackground?.let {
+            val drawable = GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, intArrayOf(0, 0))
+            drawable.alpha = 0xA0
+            it.background = drawable
+            gradientDrawable = drawable
+        }
+
+        val fabInitBg = queueAll.backgroundTintList?.defaultColor ?: 0
+        val fabInitFg = queueAll.currentTextColor
+        animator.addUpdateListener {
+            val f = it.animatedFraction
+
+            val headerBg = evaluator.evaluate(f, 0, bgColor) as Int
+            gradientDrawable?.colors = intArrayOf(headerBg, 0)
+
+            val fabBg = evaluator.evaluate(f, fabInitBg, bgColor) as Int
+            val fabFg = evaluator.evaluate(f, fabInitFg, fgColor) as Int
+            queueAll.background.setTint(fabBg)
+            queueAll.setTextColor(fabFg)
+            queueAll.iconTint = ColorStateList.valueOf(fabFg)
+        }
+        animator.duration = 400
+        animator.start()
     }
 }
