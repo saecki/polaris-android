56b698deda4b6336c5d9c20f778c13dd68003e03
saecki <tobiasschmitz2001@gmail.com>
2021-04-19 15:42:23 +0200

    add queue all button to album view

--- app/src/main/java/agersant/polaris/features/browse/BrowseViewAlbum.kt
+++ app/src/main/java/agersant/polaris/features/browse/BrowseViewAlbum.kt
@@ -2,23 +2,28 @@ package agersant.polaris.features.browse
 
 import agersant.polaris.CollectionItem
 import agersant.polaris.PlaybackQueue
-import agersant.polaris.PolarisApp
 import agersant.polaris.R
 import agersant.polaris.Song
 import agersant.polaris.api.API
 import agersant.polaris.api.ThumbnailSize
 import agersant.polaris.databinding.ViewBrowseAlbumBinding
+import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Bitmap
 import android.graphics.drawable.GradientDrawable
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.os.postDelayed
 import androidx.palette.graphics.Palette
 import androidx.recyclerview.widget.ItemTouchHelper
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 
 @SuppressLint("ViewConstructor")
 internal class BrowseViewAlbum(
@@ -34,6 +39,7 @@ internal class BrowseViewAlbum(
     private val artwork: ImageView
     private val artist: TextView
     private val title: TextView
+    private val queueAll: ExtendedFloatingActionButton
     private val divider: View?
 
     init {
@@ -45,6 +51,7 @@ internal class BrowseViewAlbum(
         artwork = binding.albumArtwork
         artist = binding.albumArtist
         title = binding.albumTitle
+        queueAll = binding.queueAll
         divider = binding.headerDivider
 
         val recyclerView = binding.browseRecyclerView
@@ -70,8 +77,10 @@ internal class BrowseViewAlbum(
             override fun onTransitionCompleted(p0: MotionLayout?, id: Int) {
                 if (id == R.id.start) {
                     title.textAlignment = TEXT_ALIGNMENT_CENTER
+                    queueAll.extend()
                 } else {
                     title.textAlignment = TEXT_ALIGNMENT_TEXT_START
+                    queueAll.shrink()
                 }
             }
 
@@ -85,6 +94,14 @@ internal class BrowseViewAlbum(
                 }
             }
         })
+
+        queueAll.setOnClickListener {
+            playbackQueue.addItems(adapter.items.filterIsInstance<Song>())
+            queueAll.setIconResource(R.drawable.ic_check_24)
+            Handler().postDelayed(1000) {
+                queueAll.setIconResource(R.drawable.ic_playlist_play_24)
+            }
+        }
     }
 
     override fun setItems(items: List<CollectionItem>) {
@@ -106,39 +123,50 @@ internal class BrowseViewAlbum(
         artist.text = artistString
         title.text = song.album ?: ""
         if (song.artwork != null) {
-            api.loadThumbnailIntoView(song, ThumbnailSize.Small, artwork) { image ->
-                Palette.from(image).generate { palette ->
-                    val swatch = palette?.run {
-                        dominantSwatch
-                            ?: mutedSwatch
-                    }
-
-                    swatch?.run {
-                        val animator = ValueAnimator.ofArgb(0, rgb)
-                        var gradientDrawable: GradientDrawable? = null
-                        headerBackground?.let {
-                            val drawable = GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, intArrayOf(0, 0))
-                            drawable.alpha = 0xA0
-                            it.background = drawable
-                            gradientDrawable = drawable
-                        }
-                        sidebarBackground?.let {
-                            val drawable = GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, intArrayOf(0, 0))
-                            drawable.alpha = 0xA0
-                            it.background = drawable
-                            gradientDrawable = drawable
-                        }
-                        animator.addUpdateListener {
-                            val bg = it.animatedValue as Int
-                            gradientDrawable?.colors = intArrayOf(bg, 0)
-                        }
-                        animator.duration = 250
-                        animator.start()
-                    }
-                }
-            }
+            api.loadThumbnailIntoView(song, ThumbnailSize.Small, artwork, this::animateAccentColorChange)
         } else {
             artwork.setImageResource(R.drawable.ic_fallback_artwork)
+        }
+    }
+
+    private fun animateAccentColorChange(image: Bitmap) {
+        Palette.from(image).generate { palette ->
+            val swatch = palette?.run {
+                dominantSwatch
+                    ?: mutedSwatch
+            }
+
+            swatch?.run {
+                val animator = ValueAnimator.ofFloat(0f, 1f)
+                val evaluator = ArgbEvaluator()
+                var gradientDrawable: GradientDrawable? = null
+                headerBackground?.let {
+                    val drawable = GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, intArrayOf(0, 0))
+                    drawable.alpha = 0xA0
+                    it.background = drawable
+                    gradientDrawable = drawable
+                }
+                sidebarBackground?.let {
+                    val drawable = GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, intArrayOf(0, 0))
+                    drawable.alpha = 0xA0
+                    it.background = drawable
+                    gradientDrawable = drawable
+                }
+                val fabInitBg = queueAll.backgroundTintList?.defaultColor ?: 0
+                val fabInitFg = queueAll.currentTextColor
+                animator.addUpdateListener {
+                    val f = it.animatedFraction
+                    val bg = evaluator.evaluate(f, 0, rgb) as Int
+                    val fabBg = evaluator.evaluate(f, fabInitBg, rgb) as Int
+                    val fabFg = evaluator.evaluate(f, fabInitFg, bodyTextColor) as Int
+                    gradientDrawable?.colors = intArrayOf(bg, 0)
+                    queueAll.background.setTint(fabBg)
+                    queueAll.setTextColor(fabFg)
+                    queueAll.iconTint = ColorStateList.valueOf(fabFg)
+                }
+                animator.duration = 400
+                animator.start()
+            }
         }
     }
 }
