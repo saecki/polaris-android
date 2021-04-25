f636893c1d766676534107d198f0930a94d650e6
saecki <tobiasschmitz2001@gmail.com>
2021-04-26 01:56:12 +0200

    refactor BrowseViewModel

--- app/src/main/java/agersant/polaris/features/browse/BrowseViewModel.kt
+++ app/src/main/java/agersant/polaris/features/browse/BrowseViewModel.kt
@@ -8,20 +8,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class BrowseViewModel : ViewModel() {
-
-    private val mItems = MutableLiveData(listOf<CollectionItem>())
-    private val mError = MutableLiveData(false)
-    private val mFetching = MutableLiveData(false)
-
-    val api = PolarisApp.state.api
-    val serverAPI = PolarisApp.state.serverAPI
-    val playbackQueue = PolarisApp.state.playbackQueue
-    val items: LiveData<List<CollectionItem>> = mItems
-    val fetching: LiveData<Boolean> = mFetching
-    val error: LiveData<Boolean> = mError
-    var initialCreation = true
-    var scrollPosition = 0
-
     private val fetchCallback = object : ItemsCallback {
         override fun onSuccess(items: List<CollectionItem>) {
             mItems.postValue(items)
@@ -35,6 +21,19 @@ class BrowseViewModel : ViewModel() {
             mError.postValue(true)
         }
     }
+    private val mItems = MutableLiveData(listOf<CollectionItem>())
+    private val mError = MutableLiveData(false)
+    private val mFetching = MutableLiveData(false)
+
+    val api = PolarisApp.state.api
+    val serverAPI = PolarisApp.state.serverAPI
+    val playbackQueue = PolarisApp.state.playbackQueue
+    val items: LiveData<List<CollectionItem>> = mItems
+    val fetching: LiveData<Boolean> = mFetching
+    val error: LiveData<Boolean> = mError
+    var initialCreation = true
+    var scrollPosition = 0
+
 
     fun loadPath(path: String) {
         mFetching.value = true
