27080488a227945140b62f350c9803c77a7b4472
Saecki <tobiasschmitz2001@gmail.com>
2021-05-13 02:53:30 +0200

    fix warnings

--- app/src/main/java/agersant/polaris/features/browse/BrowseContentAlbum.kt
+++ app/src/main/java/agersant/polaris/features/browse/BrowseContentAlbum.kt
@@ -69,7 +69,7 @@ internal class BrowseContentAlbum(
 
         when (context.resources.configuration.orientation) {
             Configuration.ORIENTATION_PORTRAIT -> {
-                recyclerView.setOnScrollChangeListener { v, _, _, _, _ ->
+                recyclerView.setOnScrollChangeListener { _, _, _, _, _ ->
                     updateDividerVisibility()
                 }
             }
@@ -99,7 +99,7 @@ internal class BrowseContentAlbum(
         queueAll.setOnClickListener {
             playbackQueue.addItems(adapter.items)
             queueAll.setIconResource(R.drawable.ic_check_24)
-            Handler().postDelayed(1000) {
+            Handler(context.mainLooper).postDelayed(1000) {
                 queueAll.setIconResource(R.drawable.ic_playlist_play_24)
             }
         }
@@ -195,7 +195,7 @@ internal class BrowseContentAlbum(
             motionLayout?.progress = 1f
             recyclerView.scrollToPosition(position - 1)
             queueAll.shrink()
-            Handler().post {
+            Handler(context.mainLooper).post {
                 updateDividerVisibility()
             }
         }
